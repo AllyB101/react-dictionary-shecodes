6d7d0799ae7ff309ad2f469ffb346df02a76e907
AllyB <allendebennett@gmail.com>
2022-11-07 19:26:28 -0600

    Update Photos.js

--- src/Photos.js
+++ src/Photos.js
@@ -9,8 +9,13 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="norefferer">
-                  <img src={photo.src.tiny} className="img-fluid" />
+                <a
+                  href={photo.src.original}
+                  target="_blank"
+                  rel="norefferer"
+                  alt="blank"
+                >
+                  <img src={photo.src.tiny} className="img-fluid" alt="blank" />
                 </a>
               </div>
             );
